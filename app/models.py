a6f4844e0c8b862ae93a21478e7932d5918a3ec2
Travis Dahlin <trdahlin@gmail.com>
2018-05-05 22:34:29 -0700

    Database add Post class to models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
+from datetime import datetime
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -8,3 +9,12 @@ class user(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post ()>'.format(self.body)

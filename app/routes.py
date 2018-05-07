f9e16f2308f8c7de856d9b864849b1c2a7880802
Travis Dahlin <trdahlin@gmail.com>
2018-05-06 23:08:29 -0700

    User Login refactor template to display username.

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def index():
 			'body': 'Wow dude, just wow...'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

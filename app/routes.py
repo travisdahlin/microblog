79b6682c29f40e74df5a2d24b0dee1929a307b6c
Travis Dahlin <trdahlin@gmail.com>
2018-08-25 16:48:17 -0700

    Correct path in edit profile route

--- app/routes.py
+++ app/routes.py
@@ -82,7 +82,7 @@ def user(username):
     return render_template('user.html', user=user, posts=posts)
 
 
-@app.route('edit_profile', methods=['GET', 'POST'])
+@app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm()
